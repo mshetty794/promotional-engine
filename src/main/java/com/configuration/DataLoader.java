dc4ace043a628c799c646a5533b2436fac467435
Mithun Shetty (mitshett) <mitshett@cisco.com>
2022-11-09 23:30:40 +0530

    Loading Data into Product Catalogue Model

--- src/main/java/com/configuration/DataLoader.java
+++ src/main/java/com/configuration/DataLoader.java
@@ -1,5 +1,6 @@
 package com.configuration;
 
+import com.dto.ProductCatalogue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -11,5 +12,10 @@ public class DataLoader {
     public static Configuration loadData() throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         return mapper.readValue(new File("src/main/resources/configuration.yaml"), Configuration.class);
+    }
+
+    public static ProductCatalogue loadProductCatalogueData() throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(new File("src/main/resources/configuration.yaml"), ProductCatalogue.class);
     }
 }
