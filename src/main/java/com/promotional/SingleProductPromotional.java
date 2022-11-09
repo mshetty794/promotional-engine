1b5e964ac81033cff9d2cc9c78a97f360561bd3d
Mithun Shetty (mitshett) <mitshett@cisco.com>
2022-11-09 19:39:19 +0530

    Promotional Rules
    - Implementation to check if Single product promotional is applicable

--- src/main/java/com/promotional/SingleProductPromotional.java
+++ src/main/java/com/promotional/SingleProductPromotional.java
@@ -2,6 +2,8 @@ package com.promotional;
 
 import com.dto.Cart;
 
+import java.util.Map;
+
 public class SingleProductPromotional implements Promotional {
 
     private int quantity;
@@ -17,6 +19,10 @@ public class SingleProductPromotional implements Promotional {
 
     @Override
     public boolean isApplicable(Cart cartDetails) {
+        for (Map.Entry<String, Integer> orderedProduct : cartDetails.getOrderedItems().entrySet()) {
+            if (orderedProduct.getKey().equalsIgnoreCase(productName) && orderedProduct.getValue() >= quantity)
+                return true;
+        }
         return false;
     }
 
